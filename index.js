d84d4f10e0ee1b4cecb8b0e1c8ca44a752909262
sunilsimar <111110522+sunilsimar@users.noreply.github.com>
2023-06-27 17:25:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const db = require('./db');
+const routes = require('./routes');
+
+dotenv.config({ path: './.env' });
+
 const app = express();
+const PORT = process.env.PORT || 2300;
 
-app.get('/',(req,res)=>{
-    res.send('Hello world1')
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs'); // Set EJS as the template engine
 
-app.get('/short',(req,res)=>{
-    res.send('Hello from short')
-})
+app.use('/', routes);
 
-app.listen(process.env.PUBLIC_PORT|| 10000,()=>{
-    console.log('Server started')
-})
+app.get('/', (req, res) => {
+  db.ShortUrl.find({}, (err, shortUrls) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    res.render('combined', { shortUrls: shortUrls }); // Pass the shortUrls data to the template
+  });
+});
+
+app.listen(PORT, () => {
+  const ServerStatus = true;
+  if (ServerStatus) {
+    console.log(`Server is running on port ${PORT}`);
+  } else {
+    console.log('Unable to connect to the server');
+  }
+});
